182f92c13ad06ce884674b91cad615d88bbd2d84
Aditi_Singh <2019aditi.a.singh@ves.ac.in>
2021-09-20 14:18:28 +0530

    removed shop.js

--- models/user.js
+++ models/user.js
@@ -23,6 +23,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    shopName: {
+        type: String
+    },
+    noOfProducts: {
+        type: String
+    },
     productId: [{ type: ObjectId, ref: "Product" }]
 })
 
